d44b9f6616531e68f3add4adc872cf840aefb7c5
Thanh <thc.huynh@protonmail.com>
2023-01-22 00:27:50 +0100

    mongoDB into the .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const mongo_user = process.env.MONGO_USER
 const mongo_password = process.env.MONGO_PASSWORD
 
-const CONNECTION_URL = `mongodb+srv://${mongo_user}:${mongo_password}@cluster0.ofi1wkl.mongodb.net/?retryWrites=true&w=majority`
+const CONNECTION_URI = process.env.MONGO_URI
 
 // const local_TestDB = "mongodb://127.0.0.1:27017/test"
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(CONNECTION_URL, {
+    const conn = await mongoose.connect(CONNECTION_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
